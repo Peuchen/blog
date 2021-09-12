6e79e421175de591e6b08f00495c17c97e3a3702
Niels Peuchen <npeuchen@gmail.com>
2021-09-12 18:43:15 +0200

    Add resources method and link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-end
-
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
